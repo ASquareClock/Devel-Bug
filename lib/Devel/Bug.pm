61a36ae342658e1cf5fb60c53b704e157b86ff6d
Kevin Shea <7_erase.garish@icloud.com>
2026-04-13 16:15:25 -0400

    Added .gitignore

--- lib/Devel/Bug.pm
+++ lib/Devel/Bug.pm
@@ -5,21 +5,195 @@ use v5.30;
 use utf8;
 use warnings;
 
-use Exporter 'import';
-our @EXPORT= qw(bug);
-
 use Term::ANSIColor; 
 use Term::Size::Perl;
 
-use List::Util  qw(pairmap);
-use Scalar::Util qw(reftype);
+use List::Util qw(pairmap);
 use Carp qw(croak carp);
 
-
-use Data::Dumper; $Data::Dumper::Indent= 1;
-use Data::Dump qw(pp);
+use Data::Dump;
 
 
+use constant BUG_OPTIONS => {
+    out      => '',
+    package  => '',
+    filename => '',
+    lineno   => '',
+    val      => '*',
+};
+
+use constant ALIAS_OPTIONS => {
+    output => 'out',
+    pkg    => 'package',
+    fn     => 'filename',
+    line   => 'lineno',
+};
+
+use constant USE_OPTIONS => { BUG_OPTIONS->%*, bug => '' };
+
+sub validate { 
+    my $params= shift;
+
+    pairmap {
+        my $a= lc $a;
+        $a= ALIAS_OPTIONS->{$a} if exists ALIAS_OPTIONS->{$a};
+
+        exists $params->{$a} or croak qq(Unknown option '$a');
+        $params->{$a} eq '*' or $params->{$a} eq ref($b) or croak qq(Option '$a' may not be type '@{[ ref($b) || '(SCALAR)' ]}');
+
+        $a => $b;
+    } @_;
+}
+
+
+our %OPTIONS;
+
+sub import {
+    shift;
+
+    %OPTIONS= ( out => *STDERR, validate USE_OPTIONS, @_ );
+
+    my $bug= 'bug';
+
+    if (exists $OPTIONS{bug}) {
+        # Don't export anything if explicity set to falsy.
+        $bug= $OPTIONS{bug} or return;
+
+        # Export bug under a different name.
+        $bug=~/^ ( [a-z]\w* | _\w+ ) $/nix or croak qq(Illegal characters in 'bug' override subroutine name '$bug');
+        delete $OPTIONS{bug};
+    }
+
+    no strict 'refs';
+    *{ caller.'::'.$bug }= \&bug;
+}
+
+
+# Debugging utility class.
+#   Allows for inlining a temporary "bug" which will output intermediate expression data.
+#   Ex:     my $infoPN= $CV_INFO_DIR."/".(bug('relpath')= substr($_, length($sourceDir) + 1));
+#   Output: relpath=(...)
+
+# To preserve list context, use form: (bug 'list')= ( some list );
+
+sub bug :lvalue {   # ($label= '', key/val paired options)
+    # Pop an even number of key/val pairs from tail of list to init object.
+    my $self= bless { %OPTIONS, validate BUG_OPTIONS, splice @_, @_ & 1 }, __PACKAGE__;
+
+    # Get sigil/label string, if there is one.
+    $self->{label}= shift // '';
+    $self->{sigil}= $self->{label}=~s{^[@%]?}{} && $&;
+
+    # Get extra info to include with output.
+    my %info;
+
+    @info{ qw(package filename lineno) }= caller;
+    $info{lineno}= "line $info{lineno}";
+
+    $self->{info}= join(' ', map { $self->{$_}? $info{$_} : () } qw(package filename lineno));
+
+    # Tie an array or scalar, for list or scalar context respectively.
+    if (wantarray) {
+        $self->{data}= [];
+
+        tie my @a, __PACKAGE__, $self;
+        return @a;
+    } else {
+        $self->{sigil} and croak qq(Sigil '$self->{sigil}' used in scalar context);
+        $self->{data}= \do { my $scalar };
+
+        tie my $s, __PACKAGE__, $self;
+        return $s;
+    }
+}
+
+sub TIESCALAR { $_[1] }
+sub TIEARRAY  { $_[1] }
+
+sub CLEAR     { $_[0]->{data}= [] }
+sub EXTEND    { $_[0]->{count}= $_[1] }
+sub FETCH     { (@_ == 1)? $_[0]->{data}->$* : $_[0]->{data}[ $_[1] ] }
+
+sub STORE {
+    my $self=     $_[0];
+    my $sigil=    $self->{sigil};
+    my $data=     $self->{data};
+
+    my $isScalar= @_ == 2;                              # was STORE called for SCALAR or ARRAY?
+    my $override= exists $self->{val};
+    my $width=    (Term::Size::Perl::chars $self->{out})[0];    # If not a terminal, $width is 0
+
+    my sub sayVal {
+        my $x= shift;
+        my $txt;
+
+        my sub valText {
+            my $color= shift;
+            my $label= $self->{label};
+            my $info=  $self->{info};
+            my $ml=    shift? "\n" : '';
+            my $txt;
+
+            my sub cv {
+                my $txt= ref($_[0])? Data::Dumper::pp($_[0]) : $_[0];
+                # $_[0]= Data::Dumper::pp($_[0]) if ref $_[0];
+                # $color? colored($_[0], 'red on_grey23') : $_[0]
+                $color? colored($txt, 'red on_grey23') : $txt;
+            }
+
+            my $items= 
+                join $ml || ' ',
+                map { $ml? "  $_" : $_ }    # multiline?
+                        ($override)?     ( cv $self->{val}                         )  # override value specified
+                    :   ($isScalar)?     ( cv $$data                               )  # data is scalar
+                    :   ($sigil eq '@')? ( map cv($_), @$data                      )  # data is list: output grouped
+                    :   ($sigil eq '%')? ( pairmap { cv($a).' => '.cv($b) } @$data )  # data is list of keys/value pairs: output grouped
+                    :                    ( cv $data->[$x]                          ); # data is list: output as individual items
+
+            $label.= '['.($color? colored($x, 'red') : $x).']'      if defined $x;
+
+            $info=  $color? colored($info, 'bold').': ' : "$info: " if length $info;
+            $label= $color? colored($label, 'bold').'=' : "$label=" if length $label;
+
+            $txt.= $info.$label;
+            $txt.= ($ml or not $color)? "($ml$items$ml)" : "$ml$items$ml";
+        }
+
+        $txt= valText;                                      # get plain text
+        $txt= valText(1, length($txt) > $width) if $width;  # get colored terminal text, accounting for width
+
+        say { $self->{out} } $txt;
+    }
+
+    if ($isScalar)
+    {
+        $$data= $_[1];
+        sayVal;
+    } else {
+        my $x=       $_[1];
+        $data->[$x]= $_[2];
+
+        ($sigil or $override)
+        ?   --$self->{count} == 0 && sayVal
+        :   sayVal($x);
+    }
+}
+
+
+# Catch if user code attempts to use this like a regular object.
+sub AUTOLOAD {
+    our $AUTOLOAD;
+    die qq(Attempt to call unneeded non-existent subroutine '$AUTOLOAD': class @{[ __PACKAGE__ ]} intended for inline logging only);
+}
+
+# Prevent AUTOLOAD from getting called for DESTROY.
+sub DESTROY { }
+
+
+
+1;
+
+# Implemented. Keeping comment because it's a good use example.
 
 # IDEA:
 #   NOW:
@@ -31,124 +205,4 @@ use Data::Dump qw(pp);
 #   subname($sub)=~/^.+(?=::)/
 #   ?   bug($&)= *{ $caller.'::'.$name }= \&{ $&.'::'.$name }
 #   :   carp qq(Unable to get package name from anonymous sub "@{[ subname($sub) ]}");
-
-
-
-# Debugging utility class.
-#   Allows for inlining a temporary "bug" which will output intermediate expression data.
-#   Ex:     my $infoPN= $CV_INFO_DIR."/".(bug('relpath')= substr($_, length($sourceDir) + 1));
-#   Output: relpath=(...)
-
-# To preserve list context, use form: (bug 'list')= ( some list );
-
-sub bug :prototype(;$) :lvalue  # ($label= '')
-{
-    my $label= shift // '';
-    my $sigil= $label=~s{^[@%]?}{} && $&;
-    
-    if (wantarray)  { tie my @a, __PACKAGE__, $label, $sigil; return @a }
-    else            { tie my $s, __PACKAGE__, $label, $sigil; return $s }
-}
-
-sub TIESCALAR   # ($class, $label, $sigil)
-{
-    $_[2] and croak qq(Sigil '$_[2]' used in scalar context);
-
-    bless { label => $_[1], data => \do { my $s }, isScalar => 1 }, $_[0];
-}
-
-sub TIEARRAY    # ($class, $label, $sigil)
-{
-    bless { label => $_[1], sigil => $_[2], data => [] }, $_[0];
-}
-
-sub CLEAR   # ($self)
-{
-    $_[0]->{data}= [];
-}
-
-sub EXTEND  # ($self, $size)
-{
-    $_[0]->{count}= $_[1] if $_[0]->{sigil};
-}
-
-sub FETCH   # ($self, $x= undef)    Maybe could count args instead of isScalar test
-{
-    $_[0]->{isScalar}
-    ?   $_[0]->{data}->$*
-    :   $_[0]->{data}[ $_[1] ];
-}
-
-sub STORE
-{
-    my $self=  shift;
-    my $sigil= $self->{sigil};
-    my $data=  $self->{data};
-    my $width= (Term::Size::Perl::chars)[0];
-
-    my sub sayVal
-    {
-        my $x= shift;
-
-        my sub valText
-        {
-            my $color= shift;
-            my $label=  $self->{label};
-            my $ml=     shift? "\n" : '';
-            local $_;
-
-            my sub cv
-            {
-                $_[0]= pp($_[0]) if ref $_[0];
-                $color? colored($_[0], 'red on_grey23') : $_[0]
-            }
-
-            my $items= 
-                join $ml || ' ',
-                map $ml? "  $_" : $_,
-                    $self->{isScalar}? ( cv $$data )
-                :       ($sigil eq '@')? map cv($_), @$data
-                    :   ($sigil eq '%')? pairmap { cv($a).' => '.cv($b) } @$data
-                    :       ( cv $data->[$x] );
-
-            $label.= '['.($color? colored($x, 'red') : $x).']' if defined $x;
-
-            $_.= $color? colored($label, 'bold').'=' : "$label=" if length $label;
-            $_.= ($ml or not $color)? "($ml$items$ml)" : "$ml$items$ml";
-            $_;
-        }
-
-        local $_= valText;
-        $_= valText(1, length > $width) if $width;
-
-        say STDERR;
-    }
-
-    if ($self->{isScalar})
-    {
-        $$data= shift;
-        sayVal;
-    }
-    else
-    {
-        my $x=       shift;
-        $data->[$x]= shift;
-
-        if ($sigil) { sayVal if --$self->{count} == 0 }
-        else        { sayVal($x) }
-    }
-}
-
-
-sub AUTOLOAD
-{
-    our $AUTOLOAD;
-
-    die qq(Attempt to call unneeded non-existent subroutine '$AUTOLOAD': class @{[ __PACKAGE__ ]} intended for inline logging only);
-}
-
-sub DESTROY { }
-
-
-1;
 
--- test.pl
+++ test.pl
@@ -9,16 +9,25 @@ use open IO => ':utf8', ':std';
 # use Sub::Util qw(subname);
 # use Scalar::Util qw(looks_like_number blessed weaken);
 
-use Carp qw(croak confess carp cluck);
+# use Carp qw(croak confess carp cluck);
 
-use Data::Dumper; $Data::Dumper::Indent= 1;
-use Data::Dump qw(pp);
-
-
-use Bug;
+# use Data::Dumper; $Data::Dumper::Indent= 1;
+# use Data::Dump qw(pp);
 
 
+use Devel::Bug out => *STDOUT, lineno => 1;
 
+say "=== Scalar Test ===";
+my $result= (my $in= bug('scalar-test')= (100 - 3)) + 7;
+say "Final scalar result: $result";
+
+say "\n=== Simple List Test ===";
+my @nums= (bug '@grouped-list-test')= (bug filename => 1)= (10, 20, 30, 40);
+say "List length: ", scalar @nums;
+
+say "\n=== Key/Value List Test ===";
+my %hash= (bug '%pairs-test')= (alpha => 1, beta => 2, gamma => 3, delta => 4);
+say "Hash has ", scalar(keys %hash), " keys";
 
 
 
